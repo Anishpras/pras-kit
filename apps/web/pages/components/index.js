1cb3548b3d95ece3f97b6fa72eea4fc7bec0b26c
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-17 10:06:42 +0530

    added component thumbnails

--- apps/web/pages/components/index.js
+++ apps/web/pages/components/index.js
@@ -1,16 +1,23 @@
 import Card from "../../components/Components/Card";
 import Nav from "../../components/Nav";
+import componentsCardData from "../../data/component";
 
 const components = () => {
   return (
     <div className="h-screen overflow-y-auto">
       <Nav />
       <div className="w-full py-8 flex flex-wrap justify-center items-center gap-8">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {componentsCardData.map(
+          ({ id, name, numberOfComponents, link, image }) => (
+            <Card
+              key={id}
+              name={name}
+              numberOfComponents={numberOfComponents}
+              link={link}
+              image={image}
+            />
+          )
+        )}
       </div>
     </div>
   );
