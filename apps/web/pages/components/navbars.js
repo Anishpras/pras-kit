c975f348f31bfea8ec713e7396fd6e5eb8f324f7
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-22 10:40:55 +0530

    header link added

--- apps/web/pages/components/navbars.js
+++ apps/web/pages/components/navbars.js
@@ -7,7 +7,7 @@ const navbars = () => {
     <div className="h-screen overflow-y-auto">
       <Nav />
       <div className="px-4 flex justify-center items-center my-2">
-        <Header />
+        <Header link="/components" name="Navbars" />
       </div>
 
       <div className="w-full py-8 flex flex-wrap justify-center items-center gap-8"></div>
