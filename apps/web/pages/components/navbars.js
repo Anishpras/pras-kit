bdc87125aff00e72c06aa37018262179755da9ed
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-22 10:38:07 +0530

    header done

--- apps/web/pages/components/navbars.js
+++ apps/web/pages/components/navbars.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from "../../components/Header";
 import Nav from "../../components/Nav";
 
 const navbars = () => {
   return (
     <div className="h-screen overflow-y-auto">
       <Nav />
+      <div className="px-4 flex justify-center items-center my-2">
+        <Header />
+      </div>
+
       <div className="w-full py-8 flex flex-wrap justify-center items-center gap-8"></div>
     </div>
   );
