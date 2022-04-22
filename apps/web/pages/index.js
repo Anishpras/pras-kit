dd4564a6bce36498b7c2d3280056fc8b3471caf0
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-22 11:11:55 +0530

    added favicon

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -3,10 +3,14 @@ import BlobThree from "../components/Blobs/BlobThree";
 import BlobTwo from "../components/Blobs/BlobTwo";
 import HeroSection from "../components/HeroSection";
 import Nav from "../components/Nav";
-
+import Head from "next/head";
 export default function Home() {
   return (
     <div className="h-screen overflow-hidden">
+      <Head>
+        <title>Pras-Kit</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
       <Nav />
       <div className="w-full ">
         <BlobOne />
