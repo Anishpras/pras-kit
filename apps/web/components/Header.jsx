82a158f4079838f76c9eb3265ad387021591c8b9
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-22 10:46:19 +0530

    padding decreased

--- apps/web/components/Header.jsx
+++ apps/web/components/Header.jsx
@@ -5,10 +5,10 @@ const Header = ({ link, name }) => {
   return (
     <div className="flex justify-center items-center bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] p-0.5 rounded-xl w-full max-w-7xl">
       <div className="bg-[#111827] w-full rounded-xl flex items-center p-2 text-white">
-        <Link href={link}>
+        <Link href={link} passHref>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10 hover:cursor-pointer"
+            className="h-10 w-10 hover:cursor-pointer hover:scale-75 transition-all ease-in-out"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
